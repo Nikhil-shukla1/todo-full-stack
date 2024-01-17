827a29200dad86c85b6054b70d925d36df407846
NIKHIL SHUKLA <nikhilshukla311@gmail.com>
2024-01-17 17:41:13 +0530

    done

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
-
+const {createTodo,updateTodo} = require('./types')
 const app = express();
 app.use(express.json());
 
 app.get('/api/v1',(req,res)=>{
-    res.json({
-        message:""
-    })
-})
-app.post('/api/v2',(req,res)=>{
 
+})
+app.post('/api/todo',(req,res)=>{
+    const createPayload = req.body;
+    const parsePayload = createTodo.safeParse(createPayload);
+
+    if(!parsePayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs",
+        })
+        return;
+    }
+});
+
+app.put('/api/update',(req,res)=>{
+    const updatePayload = req.body;
+    const parsedPayload = updatePayload.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:'you sent the wrong inputs',
+        })
+        return;
+    }
 });
 const port =3002;
 app.listen(port,(req,res)=>{
